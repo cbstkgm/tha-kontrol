1702484f8abf2e8f900ebe56274de43edcda49b7
cbs <cbs@example.com>
2026-08-19 13:35:14 +0300

    Revert vite dev fallback

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'dev-html-fallback',
-      configureServer(server) {
-        server.middlewares.use((req, _res, next) => {
-          if (req.url === '/' || req.url === '/tha-makam/' || req.url === '/tha-makam/index.html') {
-            req.url = '/tha-makam/index.dev.html';
-          }
-          next();
-        });
-      }
-    }
-  ],
+  plugins: [react()],
   base: '/tha-makam/',
 })
